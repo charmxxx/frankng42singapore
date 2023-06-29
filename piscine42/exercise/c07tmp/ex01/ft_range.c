850cd213d6a10895e5b26c47ef45d6e578b26007
Frank Nguyen <vietnguy@student.42singapore.sg>
2023-06-29 18:03:19 +0800

    Solve ex02

--- piscine42/exercise/c07tmp/ex01/ft_range.c
+++ piscine42/exercise/c07tmp/ex01/ft_range.c
@@ -1,28 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vietnguy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/29 16:11:04 by vietnguy          #+#    #+#             */
-/*   Updated: 2023/06/29 16:15:32 by vietnguy         ###   ########.fr       */
+/*   Created: 2023/06/29 16:22:39 by vietnguy          #+#    #+#             */
+/*   Updated: 2023/06/29 16:37:08 by vietnguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <stdio.h>
 
-char	*ft_strdup(char *src);
-
-int	main(int argc, char *argv[])
+int	*ft_range(int min, int max)
 {
-	char	*str;
+	int	*arr;
+	int	*arr_addr;
+	int	i;
 
-	if (argc < 1)
-		return (0);
-	str = ft_strdup(argv[1]);
-	printf("Output: \"%s\"\n", str);
-	free(str);
-	return (0);
+	if (min >= max)
+		return ((void *)0);
+	arr = (int *)malloc((max - min) * sizeof(int));
+	arr_addr = arr;
+	i = min;
+	while (i < max)
+		*arr++ = i++;
+	return (arr_addr);
 }
