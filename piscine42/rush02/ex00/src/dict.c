a9818dc313bde40f8c7f1229e661e1ed0718f9f0
Frank Nguyen <vietnguy@student.42singapore.sg>
2023-07-09 22:44:52 +0800

    Clone rush02

---
+++ piscine42/rush02/ex00/src/dict.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   dict.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vietnguy <vietnguy@student.42singapore.sg  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/08 09:26:13 by vietnguy          #+#    #+#             */
+/*   Updated: 2023/07/09 20:09:12 by vietnguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rush02.h"
+#include <stdlib.h>
+
+void	ft_printkv(long long key, char *val)
+{
+	if (key < 100)
+		ft_putstr(val);
+	else
+	{
+		ft_putstr("one ");
+		ft_putstr(val);
+	}
+}
+
+int	ft_require_comma(long long key)
+{
+	return (key == 1000 || key == 10000000 || key == 1000000000);
+}
+
+void	ft_eval(t_record *dict, int i, long long key)
+{
+	long long	mod;
+	long long	div;
+
+	div = key / dict[i].key;
+	mod = key % dict[i].key;
+	if (div == 1 && dict[i].key < 100)
+	{
+		ft_putstr(dict[i].val);
+		if (mod > 0 && mod < 10)
+			ft_putstr("-");
+	}
+	else
+	{
+		ft_eval_key(dict, div);
+		ft_putstr(" ");
+		ft_putstr(dict[i].val);
+		if (mod > 0)
+		{
+			if (ft_require_comma(dict[i].key))
+				ft_putstr(",");
+			ft_putstr(" ");
+			ft_eval_key(dict, mod);	
+		}
+	}
+}
+
+void	ft_eval_key(t_record *dict, long long key)
+{
+	int		i;
+	char	*val;
+
+	val = ft_get_val(dict, key);
+	if (val != NULL)
+		return (ft_printkv(key, val));
+	i = 0;
+	while (dict[i].key > key && dict[i].val != 0)
+		i++;
+	ft_eval(dict, i, key);
+}
---
+++ piscine42/rush02/ex00/src/strconv.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strconv.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vietnguy <vietnguy@student.42singapore.sg  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/08 09:30:48 by vietnguy          #+#    #+#             */
+/*   Updated: 2023/07/08 17:55:01 by vietnguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rush02.h"
+
+char	*ft_trim(char *str)
+{
+	while (*str == ' ')
+		str++;
+	return (str);
+}
+
+long long	ft_atoi(char *str)
+{
+	long long	sign;
+	long long	n;
+
+	str = ft_trim(str);
+	sign = 1;
+	if (*str == '-')
+	{
+		sign = -1;
+		str++;
+	}
+	else if (*str == '+')
+		str++;
+	n = 0;
+	while (*str != 0 && *str >= '0' && *str <= '9')
+	{
+		n = n * 10 + *str - '0';
+		str++;
+	}
+	return (sign * n);
+}
+
+int	ft_valid_number(char *str)
+{
+	str = ft_trim(str);
+	if (*str < '0' || *str > '9')
+		return (0);
+	while (*str >= '0' && *str <= '9')
+		str++;
+	if (*str != 0 && *str != ' ')
+		return (0);
+	while (*str == ' ')
+		str++;
+	if (*str != 0)
+		return (0);
+	return (1);
+}
