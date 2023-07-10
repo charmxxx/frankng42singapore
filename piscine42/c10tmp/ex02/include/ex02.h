ca8b842bba35aa342e342d2cc4285b9b4934a54b
Frank Nguyen <dung.frank@ntucfirstcampus.com>
2023-07-10 14:44:41 +0800

    Working on ex02

---
+++ piscine42/c10tmp/ex02/include/ex02.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex02.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vietnguy <vietnguy@student.42singapore.sg  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/10 14:34:45 by vietnguy          #+#    #+#             */
+/*   Updated: 2023/07/10 14:34:48 by vietnguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EX02_H
+# define EX02_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <libgen.h>
+# include <errno.h>
+# include <string.h>
+
+#endif
